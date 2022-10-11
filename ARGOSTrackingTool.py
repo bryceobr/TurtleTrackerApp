5538ee9a1ebfa50e3752c8cb1e8e3fb02603a606
bryceobr <bryceobrienmusic@gmail.com>
2022-10-11 13:21:08 -0400

    read data directly directly from the ARGOS file

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -23,10 +23,49 @@ obs_lat = lineData[6]     # Observation Latitude
 obs_lon = lineData[7]     # Observation Longitude
 
 # Print information to the use
-#print (f'Record {record_id} indicates Sara was seen at {obs_lat}N, and {obs_lon}W on {obs_date}. ')
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N, and {obs_lon}W on {obs_date}.")
 
 #exploring sara.txt in read mode
 fileObj = open('data/raw/sara.txt','r')
 print(fileObj.readline())
 print(fileObj.readlines())
-lineList = fileObj.readlines(); print(lineList[0])
+lineList = fileObj.readlines()
+print(lineList[0])
+fileObj.close()
+
+#writing to text files
+newFile = open('newfile.txt','w')
+newFile.write("Hello world\nIt's me")
+newFile.close()
+
+#appending text file
+open('newfile.txt','a').write("\nsee what I did here homie?")
+
+#Create a variable pointing to the data file
+file_name = 'data/raw/sara.txt'
+
+#Create a file object from the file
+file_object = open(file=file_name,mode='r')
+
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Process all lines in the ARGOS file using a for loop
+lineString = line_list[100]
+    
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
