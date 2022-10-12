3949be52da6abb170413a2adb68a739421492be2
bryceobr <bryceobrienmusic@gmail.com>
2022-10-12 14:27:08 -0400

    reverse lookup for dictionary keys from values

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,37 +9,40 @@
 # Date:   Fall 2022
 #----
 
-# Copy and paste a line of data as the lineString variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY):")
 
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split()
+# # Copy and paste a line of data as the lineString variable value
+# lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
 
-# Assign variables to specfic items in the list
-record_id = lineData[0]   # ARGOS tracking record ID
-obs_date = lineData[2]   # Observation date
-ob_lc = lineData[4]       # Observation Location Class
-obs_lat = lineData[6]     # Observation Latitude
-obs_lon = lineData[7]     # Observation Longitude
+# # Use the split command to parse the items in lineString into a list object
+# lineData = lineString.split()
 
-# Print information to the use
-print (f"Record {record_id} indicates Sara was seen at {obs_lat}N, and {obs_lon}W on {obs_date}.")
+# # Assign variables to specfic items in the list
+# record_id = lineData[0]   # ARGOS tracking record ID
+# obs_date = lineData[2]   # Observation date
+# ob_lc = lineData[4]       # Observation Location Class
+# obs_lat = lineData[6]     # Observation Latitude
+# obs_lon = lineData[7]     # Observation Longitude
 
-#exploring sara.txt in read mode
-fileObj = open('data/raw/sara.txt','r')
-print(fileObj.readline())
-print(fileObj.readlines())
-lineList = fileObj.readlines()
-print(lineList[0])
-fileObj.close()
+# # Print information to the use
+# print (f"Record {record_id} indicates Sara was seen at {obs_lat}N, and {obs_lon}W on {obs_date}.")
 
-#writing to text files
-newFile = open('newfile.txt','w')
-newFile.write("Hello world\nIt's me")
-newFile.close()
+# #exploring sara.txt in read mode
+# fileObj = open('data/raw/sara.txt','r')
+# print(fileObj.readline())
+# print(fileObj.readlines())
+# lineList = fileObj.readlines()
+# print(lineList[0])
+# fileObj.close()
 
-#appending text file
-open('newfile.txt','a').write("\nsee what I did here homie?")
+# #writing to text files
+# newFile = open('newfile.txt','w')
+# newFile.write("Hello world\nIt's me")
+# newFile.close()
+
+# #appending text file
+# open('newfile.txt','a').write("\nsee what I did here homie?")
 
 #Create a variable pointing to the data file
 file_name = 'data/raw/sara.txt'
@@ -89,3 +92,20 @@ for lineString in line_list:
 #print (location_dict['20616'])
 #print(date_dict['24719'])
 print(len(location_dict))
+
+#Create an empty key list
+matching_keys = []
+
+# Loop through all key, value pairs in the date_dictionary
+for the_key, the_value in date_dict.items():
+    #See if the date (the value) matches the user date
+    if the_value == user_date:
+        matching_keys.append(the_key)
+
+#just for shits
+print(matching_keys)
+
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
